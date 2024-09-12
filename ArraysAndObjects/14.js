611c9e5c6142b69053baf53a5ea7dab6f0bf6b70
itsmegc <gc.27@outlook.com>
2024-09-12 19:55:58 +0530

    12-09-2024

---
+++ ArraysAndObjects/14.js
@@ -0,0 +1,14 @@
+// Objects
+// how to create object
+
+const person = { name: "Bruce wayne", age: 31, hobbies: ["Guitar", "MMA"] };
+// console.log(person);
+// console.log(person.name);
+// console.log(person.age);
+// console.log(person.hobbies);
+
+person.gender = "male";
+
+console.log(person);
+// how to access data from objects
+console.log(person["name"]);
