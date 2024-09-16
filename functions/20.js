07d322e1890a2f003314575df2a13b1dcc452e9a
itsmegc <gc.27@outlook.com>
2024-09-16 20:14:45 +0530

    basic functions

--- functions/20.js
+++ functions/20.js
@@ -15,3 +15,16 @@ let num2 = 8;
 //   console.log(`addition of ${num1} and ${num2} is ${addTwoNumber(num1,num2)}`);
 const returnValue = addTwoNumber(num1, num2);
 console.log(`addition of ${num1} and ${num2} is ${returnValue}`);
+
+// even odd
+
+function isEven(num) {
+  return num % 2 == 0;
+}
+
+console.log(isEven(4));
+
+function firstChar(anyString){
+    return anyString[0];
+}
+console.log(firstChar('Gaurav'));
