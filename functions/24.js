6f05a219276335ca919336879ff799a8f88e3cd6
itsmegc <gc.27@outlook.com>
2024-09-18 20:16:40 +0530

    block scope vs function scope and default parameter

---
+++ functions/24.js
@@ -0,0 +1,30 @@
+// block scope vs function scope
+
+// let and const are block scope
+// var is function scope
+
+{
+  let firstNames = "Mohit";
+  console.log(firstNames);
+}
+// console.log(firstNames) // Uncaught ReferenceError: firstName is not defined
+
+{
+  var firstName = "Gaurav";
+}
+console.log(firstName);
+
+if (true) {
+  let number = 10;
+  console.log(number);
+}
+// console.log(number); // Uncaught ReferenceError: number is not defined
+
+
+// default parameter
+
+function addTwoNumber(a=1,b=2){
+    return a+b
+}
+
+console.log(`sum of two number is ${addTwoNumber()}`)
