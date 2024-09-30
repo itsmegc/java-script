b2d8b93d65fe02aaa68656c0807047bed72e5dbf
itsmegc <gc.27@outlook.com>
2024-09-30 19:10:46 +0530

    git

--- OOP/52.js
+++ OOP/52.js
@@ -7,4 +7,22 @@ const obj2 = {
   key3: "value2",
 };
 
+<<<<<<< HEAD
 console.log(obj2);
+=======
+
+const users = [
+  createUser("Sam", "Singh", "sam.singh@yy.com", 17, "Akshya Nagar 1st Block 1st Cross, Rammurthy Nagar, Bangalore-560016"),
+  createUser("John", "Doe", "john.doe@xy.com", 28, "MG Road 2nd Block, Indiranagar, Bangalore-560038"),
+  createUser("Ravi", "Kumar", "ravi.kumar@zz.com", 42, "Jayanagar 3rd Block, JP Nagar, Bangalore-560041"),
+];
+
+// Output
+users.forEach(user => {
+  console.log(user.about());
+  console.log(`Is ${user.firstName} 18 or older? ${user.is18()}`);
+  if (user.is18()) {
+    console.log(user.sing());
+  }
+});
+>>>>>>> 0358815 (git)
