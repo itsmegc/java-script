d60db071bb5ec955ca6926a5d10f80654aa2f4da
Gaurav-CloudActiveLabs <gaurav.chand@cloudactivelabs.com>
2024-09-30 19:07:26 +0530

    30-09-2024

--- OOP/52.js
+++ OOP/52.js
@@ -1,10 +1,37 @@
-const obj1 = {
-  key1: "value1",
-  key2: "value2",
+const useMethod = {
+  about() {
+    return `${this.firstName} ${this.lastName} age is ${this.age}, email id is ${this.email}, and address is ${this.address}`;
+  },
+  is18() {
+    return this.age >= 18;
+  },
+  sing() {
+    return "Kitni dafa";
+  },
 };
 
-const obj2 = {
-  key3: "value2",
-};
+function createUser(firstName, lastName, email, age, address) {
+  return {
+    firstName,
+    lastName,
+    email,
+    age,
+    address,
+    ...useMethod, // Spread operator to include methods from useMethod
+  };
+}
 
-console.log(obj2);
+const users = [
+  createUser("Sam", "Singh", "sam.singh@yy.com", 17, "Akshya Nagar 1st Block 1st Cross, Rammurthy Nagar, Bangalore-560016"),
+  createUser("John", "Doe", "john.doe@xy.com", 28, "MG Road 2nd Block, Indiranagar, Bangalore-560038"),
+  createUser("Ravi", "Kumar", "ravi.kumar@zz.com", 42, "Jayanagar 3rd Block, JP Nagar, Bangalore-560041"),
+];
+
+// Output
+users.forEach(user => {
+  console.log(user.about());
+  console.log(`Is ${user.firstName} 18 or older? ${user.is18()}`);
+  if (user.is18()) {
+    console.log(user.sing());
+  }
+});
---
+++ OOP/53.js
@@ -0,0 +1,10 @@
+const obj1 = {
+  key1: "value1",
+  key2: "value2",
+};
+
+const obj2 = {
+  key3: "value2",
+};
+
+console.log(obj2);
