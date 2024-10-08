69fc7bd5daf70abfed802663769fd727645d2a52
itsmegc <gc.27@outlook.com>
2024-10-08 20:26:51 +0530

    08-10-2024

--- OOP/45.js
+++ OOP/45.js
@@ -13,6 +13,36 @@
 function personInfo() {
   console.log(`person name is ${this.firstName} and person age is ${this.age}`);
 }
+const personX = {
+  firstName: "James",
+  age: 55,
+  about: personInfo,
+};
+
+const personY = {
+  firstName: "Mohit",
+  age: 17,
+  about: personInfo,
+};
+
+const personZ = {
+  firstName: "Rahul",
+  age: 77,
+  about: personInfo,
+};
+
+personX.about();
+personY.about();
+personZ.about();
+
+// examples
+
+// Method for person info
+function personInfo() {
+  console.log(`person name is ${this.firstName} and person age is ${this.age}`);
+}
+
+// Object instances
 const person1 = {
   firstName: "James",
   age: 55,
@@ -31,6 +61,56 @@ const person3 = {
   about: personInfo,
 };
 
+const person4 = {
+  firstName: "Sarah",
+  age: 29,
+  about: personInfo,
+};
+
+const person5 = {
+  firstName: "Emma",
+  age: 34,
+  about: personInfo,
+};
+
+const person6 = {
+  firstName: "Daniel",
+  age: 45,
+  about: personInfo,
+};
+
+const person7 = {
+  firstName: "Jessica",
+  age: 21,
+  about: personInfo,
+};
+
+const person8 = {
+  firstName: "Michael",
+  age: 62,
+  about: personInfo,
+};
+
+const person9 = {
+  firstName: "Sophie",
+  age: 40,
+  about: personInfo,
+};
+
+const person10 = {
+  firstName: "David",
+  age: 38,
+  about: personInfo,
+};
+
+// Calling the about method for each person
 person1.about();
 person2.about();
 person3.about();
+person4.about();
+person5.about();
+person6.about();
+person7.about();
+person8.about();
+person9.about();
+person10.about();
--- OOP/47.js
+++ OOP/47.js
@@ -7,28 +7,84 @@
 // myFun.call();
 
 // call
-// Example
+// Function to display user information
 function about(hobby, favSinger) {
   console.log(this.name, this.age, hobby, favSinger);
 }
+
+// User objects
 const user1 = {
-  name: "gaurav",
+  name: "Gaurav",
   age: 8,
 };
 
 const user2 = {
-  name: "ankit",
+  name: "Ankit",
   age: 9,
 };
 
+const user3 = {
+  name: "Priya",
+  age: 12,
+};
+
+const user4 = {
+  name: "Ravi",
+  age: 15,
+};
+
+const user5 = {
+  name: "Sita",
+  age: 20,
+};
+
+const user6 = {
+  name: "Neha",
+  age: 30,
+};
+
+const user7 = {
+  name: "Rahul",
+  age: 28,
+};
+
+const user8 = {
+  name: "Pooja",
+  age: 35,
+};
+
+const user9 = {
+  name: "Karan",
+  age: 40,
+};
+
+const user10 = {
+  name: "Nisha",
+  age: 22,
+};
+
+// Using call
 about.call(user1, "sleeping", "kr$na");
 about.call(user2, "reading", "kr$na");
+about.call(user3, "painting", "A.R. Rahman");
+about.call(user4, "traveling", "Lata Mangeshkar");
+about.call(user5, "cooking", "Sonu Nigam");
+about.call(user6, "dancing", "Shreya Ghoshal");
+about.call(user7, "gaming", "Badshah");
+about.call(user8, "photography", "Arijit Singh");
+about.call(user9, "writing", "Asha Bhosle");
+about.call(user10, "swimming", "Himesh Reshammiya");
 
-// apply
+// Using apply
+about.apply(user1, ["sleeping", "kr$na"]);
+about.apply(user2, ["reading", "kr$na"]);
+about.apply(user3, ["drawing", "Taylor Swift"]);
 
-about.apply(user1, ["sleeping", "kr$na"]); //  send in array
+// Using bind
+const func1 = about.bind(user1, "sleeping", "kr$na");
+const func2 = about.bind(user2, "reading", "kr$na");
+const func3 = about.bind(user3, "dancing", "Ed Sheeran");
 
-// bind
-
-const func = about.bind(user1, "sleeping", "kr$na"); // return function
-func();
+func1(); // Output: Gaurav 8 sleeping kr$na
+func2(); // Output: Ankit 9 reading kr$na
+func3(); // Output: Priya 12 dancing Ed Sheeran
