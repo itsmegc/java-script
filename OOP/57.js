c2225ccaa9a345a19fc2eac2c4bef71db5976d7e
itsmegc <gc.27@outlook.com>
2024-10-10 20:02:03 +0530

    10-10-2024

--- OOP/57.js
+++ OOP/57.js
@@ -1,16 +1,12 @@
 function CreateUsers(firstName, lastName, email, age, address) {
-  // const user = Object.create(useMethod);
-  // const user = Object.create(CreateUsers.prototype);
   this.firstName = firstName;
   this.lastName = lastName;
   this.email = email;
   this.age = age;
   this.address = address;
-  // user.about = useMethod.about;
-  // user.is18 = useMethod.is18;
-  // user.sing = useMethod.sing;
 }
 
+// User examples
 const user1 = new CreateUsers(
   "Sam",
   "Singh",
@@ -35,12 +31,42 @@ const user3 = new CreateUsers(
   "Jayanagar 3rd Block, JP Nagar, Bangalore-560041"
 );
 
-// console.log(CreateUsers.prototype)
+// Additional user examples
+const user4 = new CreateUsers(
+  "Alice",
+  "Johnson",
+  "alice.johnson@ab.com",
+  22,
+  "Koramangala 4th Block, Bangalore-560034"
+);
 
+const user5 = new CreateUsers(
+  "Michael",
+  "Smith",
+  "michael.smith@bc.com",
+  30,
+  "Indiranagar 1st Block, Bangalore-560038"
+);
+
+const user6 = new CreateUsers(
+  "Emma",
+  "Williams",
+  "emma.williams@cd.com",
+  19,
+  "Whitefield 1st Stage, Bangalore-560066"
+);
+
+const user7 = new CreateUsers(
+  "David",
+  "Brown",
+  "david.brown@de.com",
+  45,
+  "HSR Layout 3rd Sector, Bangalore-560102"
+);
+
+// Prototype methods
 CreateUsers.prototype.about = function () {
-  return `${this.firstName + " " + this.lastName} age is ${
-    this.age
-  } email id is ${this.email} and address is ${this.address}`;
+  return `${this.firstName + " " + this.lastName} age is ${this.age} email id is ${this.email} and address is ${this.address}`;
 };
 
 CreateUsers.prototype.is18 = function () {
@@ -51,8 +77,17 @@ CreateUsers.prototype.sing = function () {
   return "Kitni dafa";
 };
 
+// Example usage
+const users = [user1, user2, user3, user4, user5, user6, user7];
+
+// Log user details and check if they are 18 or older
+users.forEach((user) => {
+  console.log(user.about());
+  console.log(`Is ${user.firstName} 18 or older? ${user.is18()}`);
+});
+
+// Output the keys of user1
 for (let key in user1) {
-  // console.log(key)
   if (user1.hasOwnProperty(key)) {
     console.log(key); // if do not want prototype property
   }
