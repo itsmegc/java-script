90331955ae05204e3b73ed8008aa97d8149b09e4
itsmegc <gc.27@outlook.com>
2024-10-16 00:23:05 +0530

    static methods and property

--- OOP/65.js
+++ OOP/65.js
@@ -1,21 +1,53 @@
 // static methods and property
 
 class Person {
-  constructor(firstName, lastName, age) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.age = age;
+    constructor(firstName, lastName, age) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.age = age;
+    }
+  
+    // Static method: class-level method
+    static classInfo() {
+      return "This is the Person class";
+    }
+  
+    // Static property
+    static species = "Homo sapiens";
+  
+    // Getter for fullName
+    get fullName() {
+      return `${this.firstName} ${this.lastName}`;
+    }
+  
+    // Setter for fullName
+    set fullName(name) {
+      const [firstName, lastName] = name.split(" ");
+      this.firstName = firstName;
+      this.lastName = lastName;
+    }
+  
+    eat() {
+      return `${this.firstName} is eating`;
+    }
+  
+    isSuperCute() {
+      return this.age < 2;
+    }
+  
+    isCute() {
+      return true;
+    }
   }
-
-  // Getter for fullName
-  get fullName() {
-    return `${this.firstName} ${this.lastName}`;
-  }
-
-  // Setter for fullName
-  set fullName(name) {
-    const [firstName, lastName] = name.split(" ");
-    this.firstName = firstName;
-    this.lastName = lastName;
-  }
-}
+  
+  const person1 = new Person("John", "Doe", 30);
+  
+  // Access instance methods
+  console.log(person1.eat()); // Output: "John is eating"
+  
+  // Access static method directly on the class
+  console.log(Person.classInfo()); // Output: "This is the Person class"
+  
+  // Access static property directly on the class
+  console.log(Person.species); // Output: "Homo sapiens"
+  
