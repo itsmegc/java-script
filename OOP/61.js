729c3e036bde831cff39b54a2aa3c25baa65c450
itsmegc <gc.27@outlook.com>
2024-10-15 23:57:16 +0530

    inheritance 2.0

---
+++ OOP/61.js
@@ -0,0 +1,31 @@
+class Animals {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+    }
+    eat() {
+      return `${this.name} is eating`;
+    }
+    isSuperCute() {
+      return this.age < 2;
+    }
+    isCute() {
+      return true;
+    }
+  }
+  
+  
+  class Dog extends Animals {
+    constructor(name, age,speed) {
+        super(name, age)
+        this.speed = speed;
+      }
+      run(){
+        return `${this.name} is running at ${this.speed}`
+      }
+  }
+  
+  
+  const tommy = new Dog("tommy", 3,45);
+  console.log(tommy)
+  console.log(tommy.run())
