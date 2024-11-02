113912171518dfa70b0e75a618451c1a664d02da
itsmegc <gc.27@outlook.com>
2024-11-02 17:38:47 +0530

    02-11-2024

---
+++ HowJavaScriptWorks/hjw_6.js
@@ -0,0 +1,10 @@
+console.log("hello world");
+let firstName = "Gaurav";
+let lastName = "Chand";
+
+const myFunction = function () {
+  let var1 = "First Variable";
+  let var2 = "Second Variable";
+  console.log(var1);
+  console.log(var2);
+};
