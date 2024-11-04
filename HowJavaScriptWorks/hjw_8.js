fbf781d9ab735b3d49255969a1144180dc73a58b
itsmegc <gc.27@outlook.com>
2024-11-04 18:52:32 +0530

    04-11-2024

--- HowJavaScriptWorks/hjw_8.js
+++ HowJavaScriptWorks/hjw_8.js
@@ -9,3 +9,63 @@ const printName = function () {
 };
 
 printName();
+
+// lexical environment, scope chain
+
+const country = "Japan";
+
+function displayCity() {
+  const city = "Tokyo";
+  console.log(city);
+  console.log(country);
+}
+
+displayCity();
+
+// lexical environment, scope chain
+
+const mainTopic = "JavaScript";
+
+const showDetails = function () {
+  const subTopic = "Closures";
+  console.log(subTopic);
+  console.log(mainTopic);
+};
+
+showDetails();
+
+// lexical environment, scope chain
+
+const language = "Python";
+
+function showFramework() {
+  const framework = "Django";
+  console.log(framework);
+  console.log(language);
+}
+
+showFramework();
+
+// lexical environment, scope chain
+
+const department = "Engineering";
+
+const describeRole = function () {
+  const role = "Software Developer";
+  console.log(role);
+  console.log(department);
+};
+
+describeRole();
+
+// lexical environment, scope chain
+
+const planet = "Earth";
+
+function displayDetails() {
+  const region = "Asia";
+  console.log(region);
+  console.log(planet);
+}
+
+displayDetails();
