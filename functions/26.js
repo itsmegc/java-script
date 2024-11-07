6662d8dbb950ecdd74085e5e1fdecb61ae573d03
itsmegc <gc.27@outlook.com>
2024-11-07 19:13:06 +0530

    07-11-2024

--- functions/26.js
+++ functions/26.js
@@ -17,3 +17,59 @@ function myFun1(name) {
 }
 
 myFun(myFun1);
+
+function greet(callback) {
+  console.log("Greeting:");
+  callback("Alice");
+}
+
+function sayHello(name) {
+  console.log("Hello", name);
+}
+
+greet(sayHello);
+
+
+function fetchData(callback) {
+  console.log("Fetching data...");
+  callback({ id: 1, name: "Product A" });
+}
+
+function handleData(data) {
+  console.log("Data received:", data);
+}
+
+fetchData(handleData);
+
+function processOrder(callback) {
+  console.log("Processing order...");
+  callback("Order #1234");
+}
+
+function confirmOrder(orderId) {
+  console.log("Order confirmed:", orderId);
+}
+
+processOrder(confirmOrder);
+
+function calculate(callback) {
+  console.log("Performing calculation...");
+  callback(5, 10);
+}
+
+function sum(a, b) {
+  console.log("Sum:", a + b);
+}
+
+calculate(sum);
+
+function logMessage(callback) {
+  console.log("Logging message...");
+  callback("Operation successful");
+}
+
+function showMessage(message) {
+  console.log("Message:", message);
+}
+
+logMessage(showMessage);
